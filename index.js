f8c8ca658dd22288b5f9f28ab7ae10daa268cfe9
Legaljay <adesinaayobami67@gmail.com>
2023-09-12 23:04:27 +0100

    progress

--- index.js
+++ index.js
@@ -73,37 +73,37 @@ const campGrounds = [
     {
       name: "Pine Ridge Campground",
       description: "Nestled deep in the forest, Pine Ridge Campground offers a serene escape for nature enthusiasts. Enjoy hiking trails, birdwatching, and peaceful nights under the starry sky.",
-      image: "/images/pexels-sagui-andrea-618848.jpg",
+      image: "./images/pexels-sagui-andrea-618848.jpg",
       button: "View Campground"
     },
     {
       name: "Lakeview Campsite",
       description: "Situated on the shores of a pristine mountain lake, Lakeview Campsite is a water lover's paradise. Fish, swim, and kayak during the day, and gather around the campfire at night.",
-      image: "/images/pexels-robert-forever-ago-2516423.jpg",
+      image: "./images/pexels-robert-forever-ago-2516423.jpg",
       button: "View Campground"
     },
     {
       name: "Mountain Haven Retreat",
       description: "Experience the breathtaking beauty of the mountains at Mountain Haven Retreat. This campground is a haven for hikers, with numerous trails that lead to panoramic vistas.",
-      image: "/images/pexels-robert-forever-ago-2496880.jpg",
+      image: "./images/pexels-robert-forever-ago-2496880.jpg",
       button: "View Campground"
     },
     {
       name: "Riverbend Campground",
       description: "Camp along the scenic riverbanks at Riverbend Campground. Listen to the soothing sounds of the river as you relax by your campfire and enjoy water activities like tubing and fishing.",
-      image: "/images/pexels-matthew-devries-2526025.jpg",
+      image: "./images/pexels-matthew-devries-2526025.jpg",
       button: "View Campground"
     },
     {
       name: "Desert Oasis Campground",
       description: "Discover the wonders of the desert at Desert Oasis Campground. With its unique landscapes and cactus-studded terrain, it's a perfect spot for stargazing and off-roading adventures.",
-      image: "/images/pexels-guduru-ajay-bhargav-939723.jpg",
+      image: "./images/pexels-guduru-ajay-bhargav-939723.jpg",
       button: "View Campground"
     },
     {
       name: "Seaside Cove Campground",
       description: "Seaside Cove Campground offers a coastal camping experience with breathtaking ocean views. Explore tide pools, go beachcombing, and fall asleep to the sound of crashing waves.",
-      image: "/images/pexels-cliford-mervil-2398220.jpg",
+      image: "./images/pexels-cliford-mervil-2398220.jpg",
       button: "View Campground"
     },
   ];
